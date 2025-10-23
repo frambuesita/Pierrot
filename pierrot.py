a7bd97928eea1fa5390e1da53b960e995a3f716e
frambuesita <adriperezdomin@gmail.com>
2025-10-23 15:58:33 +0200

    Adapting project to arquitecture

--- pierrot.py
+++ pierrot.py
@@ -1,8 +1,8 @@
-from src.classes import *
-from logic.analyzer import *
+from src.classes import Card, Hand
+from logic.analyzer import analyze, load_deck
 from logic.fuzzy import compute_bet
 
-
+card = Card("Hearts", "Ace", 14)
 deck = load_deck()
 
 Pierrot_hand = Hand() 
@@ -27,7 +27,7 @@ print("\n\nCartas totales de Pierrot (Personales + mesa):")
 Pierrot_hand.add_community_cards(community_cards)
 Pierrot_hand.show_hand()
 
-#pierrot_potential = analyze(Pierrot_hand.cards_list)
+pierrot_potential = analyze(Pierrot_hand.cards_list)
 #table_potential = analyze(community_hand)
 
 print(compute_bet(95, 80))
